38d268bdce80dfe35d796cbb7fb7fecfd5973d0b
Skye <skye@domos.me.uk>
2020-04-23 18:21:54 +0100

    allow rolling multiple at once

--- projects/namegen/namegen.js
+++ projects/namegen/namegen.js
@@ -1,18 +1,24 @@
+window.letterNo = 0;
+window.letters = [];
+window.jumbles = [];
+
 function generateLetter() {
-    var newLetter = document.createElement('div');
-    newLetter.setAttribute('class','letter');
-    jumble = setInterval(function(){
-        newLetter.innerHTML = Math.ceil(Math.random() * 26);
+    var letterNo = window.letterNo;
+    window.letterNo += 1;
+    window.letters[letterNo] = document.createElement('div');
+    window.letters[letterNo].setAttribute('class','letter');
+    window.jumbles[letterNo] = setInterval(function(){
+        window.letters[letterNo].innerHTML = Math.ceil(Math.random() * 26);
     },70);
     setTimeout(function(){
-        clearInterval(jumble);
-        var num = newLetter.innerHTML;
+        clearInterval(window.jumbles[letterNo]);
+        var num = window.letters[letterNo].innerHTML;
         console.log(num);
         var letter = String.fromCharCode(96 + parseInt(num));
-        newLetter.innerHTML = num + "<br />" + letter;
+        window.letters[letterNo].innerHTML = num + "<br />" + letter;
     },800);
-    $('#namefield').append(newLetter);
-    $(newLetter).fadeIn(500).css('display','inline-block');
+    $('#namefield').append(window.letters[letterNo]);
+    $(window.letters[letterNo]).fadeIn(500).css('display','inline-block');
 }
 
 window.onload = function() {
